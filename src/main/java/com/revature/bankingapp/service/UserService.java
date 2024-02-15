669fe90822cbf7f6142f8b22e6de570a8e579ad0
Kendra <kendracodesio@gmail.com>
2024-02-15 14:33:27 -0800

    Updates UserService class

--- src/main/java/com/revature/bankingapp/service/UserService.java
+++ src/main/java/com/revature/bankingapp/service/UserService.java
@@ -1,4 +1,32 @@
 package com.revature.bankingapp.service;
 
+import com.revature.bankingapp.DAO.UserRepository;
+import com.revature.bankingapp.exception.AccessDeniedException;
+import com.revature.bankingapp.exception.UsernameAlreadyExistsException;
+import com.revature.bankingapp.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public User registerNewUser(User newUser) {
+        if (userRepository.findByUsername(newUser.getUsername()).isPresent()) {
+            throw new UsernameAlreadyExistsException("Username already exists");
+        }
+        return userRepository.save(newUser);
+    }
+
+
+    public User login(User user) {
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword())
+                .orElseThrow(() -> new AccessDeniedException("Invalid username or password"));
+    }
 }
+
