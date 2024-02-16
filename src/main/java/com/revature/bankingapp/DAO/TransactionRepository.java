9fa3474d5709e506ad96cad86c610840ee94b925
Kendra <kendracodesio@gmail.com>
2024-02-16 14:09:23 -0800

    Completes getAccountTransactionsByDate method in Transaction class

--- src/main/java/com/revature/bankingapp/DAO/TransactionRepository.java
+++ src/main/java/com/revature/bankingapp/DAO/TransactionRepository.java
@@ -5,16 +5,24 @@ import com.revature.bankingapp.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Date;
 import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface TransactionRepository  extends JpaRepository<Transaction, Integer> {
-    // view To-Do's associated with the users account
-    List<Transaction> findAllByCreatedBy(Account account);
 
-    @Query(value = "SELECT * FROM transactions WHERE date BETWEEN :fromDate AND :toDate", nativeQuery = true)
-    List<Transaction> findByDateBetween(@Param("fromDate") Date fromDate, @Param("toDate") Date toDate);
+    @Query("SELECT t FROM Transaction t WHERE t.account.id = :accountId AND t.date BETWEEN .fromDate AND :toDate")
+    List<Transaction> findByAccountIdAndDateBetween(
+            @Param("accountId") Integer accountId,
+            @Param("fromDate") LocalDateTime fromDate,
+            @Param("toDate") LocalDateTime toDate);
+
+
+
+//    @Query(value = "SELECT * FROM transactions WHERE date BETWEEN :fromDate AND :toDate", nativeQuery = true)
+//    List<Transaction> findByDateBetween(@Param("fromDate") Date fromDate, @Param("toDate") Date toDate);
 
 }
