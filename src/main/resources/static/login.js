8671b64716835e22a80d552cbdc83627c4cf2872
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-20 09:23:15 -0600

    finished login.js (added error message handling)

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -7,10 +7,7 @@ form.addEventListener('submit', (e)=>{
     loginUser();
 })
 
-const button = document.getElementById("loginButton");
-
-
-
+// const button = document.getElementById("loginButton");
 
 // test .onclick function
 // function logUser () {
@@ -26,22 +23,78 @@ const button = document.getElementById("loginButton");
 // }
 
 // loginUser
-async function loginUser () {
+// async function loginUser () {
 
+//     let userName = document.getElementById("inputUsername").value;
+//     let passWord = document.getElementById("inputPassword").value;
+
+//     const res = await fetch(`${url}/login`, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//             username: userName,
+//             password: passWord
+//         })
+//     })
+//     .catch(e => {
+//         throw(
+//             console.log("wrong username or password", e)
+//             );
+//     });
+    
+//     const loginResponse = await res.json();
+//     console.log(loginResponse);
+
+// }
+
+
+async function loginUser () {
+  
     let userName = document.getElementById("inputUsername").value;
     let passWord = document.getElementById("inputPassword").value;
+    let errorMessage = document.getElementById("errorMessage");
 
-    const res = await fetch(`${url}/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: userName,
-            password: passWord
+    try { 
+        const res = await fetch(`${url}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: userName,
+                password: passWord
+            })
         })
-    });
-    const loginResponse = await res.json();
-    console.log(loginResponse);
+        const loginResponse = await res.json();
+        console.log(loginResponse);
+        errorMessage.style.display = "none";
+    } catch (e) {
+        console.error("wrong username or password: " + e);
+        console.error("wrong username or password");
+        errorMessage.style.display = "block";
+    }
 
 }
+
+// async function loginUser () {
+  
+//     let userName = document.getElementById("inputUsername").value;
+//     let passWord = document.getElementById("inputPassword").value;
+
+//         const res = await fetch(`${url}/login`, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({
+//                 username: userName,
+//                 password: passWord
+//             })
+//         })
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+//         .catch(error => console.log("wrong username or password: " + error))
+
+// }
