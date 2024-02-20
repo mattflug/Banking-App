7f5f69529007f3aa33929a6d85409900e1b41a16
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-20 16:22:55 -0600

    cleaned up code

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -6,49 +6,6 @@ form.addEventListener('submit', (e)=>{
     loginUser();
 })
 
-// const button = document.getElementById("loginButton");
-
-// test .onclick function
-// function logUser () {
-
-//     let userName = document.getElementById("inputEmail").value;
-//     let passWord = document.getElementById("inputPassword").value;
-//     let userObj = {
-//         userName,
-//         passWord
-//     };
-
-//     console.log(userObj);
-// }
-
-// loginUser
-// async function loginUser () {
-
-//     let userName = document.getElementById("inputUsername").value;
-//     let passWord = document.getElementById("inputPassword").value;
-
-//     const res = await fetch(`${url}/login`, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({
-//             username: userName,
-//             password: passWord
-//         })
-//     })
-//     .catch(e => {
-//         throw(
-//             console.log("wrong username or password", e)
-//             );
-//     });
-    
-//     const loginResponse = await res.json();
-//     console.log(loginResponse);
-
-// }
-
-
 async function loginUser () {
   
     let userName = document.getElementById("inputUsername").value;
@@ -79,24 +36,3 @@ async function loginUser () {
     }
 
 }
-
-// async function loginUser () {
-  
-//     let userName = document.getElementById("inputUsername").value;
-//     let passWord = document.getElementById("inputPassword").value;
-
-//         const res = await fetch(`${url}/login`, {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 username: userName,
-//                 password: passWord
-//             })
-//         })
-//         .then(res => res.json())
-//         .then(data => console.log(data))
-//         .catch(error => console.log("wrong username or password: " + error))
-
-// }
