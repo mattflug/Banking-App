e3b77df73ffab41200fcadb87ba0f8ce03e1d828
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-20 15:55:57 -0600

    create user function

--- src/main/resources/static/createAccount.js
+++ src/main/resources/static/createAccount.js
@@ -6,27 +6,6 @@ form.addEventListener('submit', (e)=>{
     createUser();
 })
 
-// test on submit function
-// function createUser () {
-
-//     let userName = document.getElementById("inputUsername").value;
-//     let passWord = document.getElementById("inputPassword").value;
-//     let firstName = document.getElementById("inputFirstName").value;
-//     let lastName = document.getElementById("inputLastName").value;
-//     let taxId = document.getElementById("inputTaxId").value;
-//     let isAdmin = document.getElementById("isAdmin").value;
-//     let userObj = {
-//         userName,
-//         passWord,
-//         firstName,
-//         lastName,
-//         taxId,
-//         isAdmin
-//     };
-
-//     console.log(userObj);
-// }
-
 async function createUser () {
   
     let userName = document.getElementById("inputUsername").value;
