a3f48681ded9876621ecf56fae760c4e4010e179
Kendra <kendracodesio@gmail.com>
2024-02-22 15:52:55 -0800

    changes made to account front-end

--- src/main/resources/static/userAccount.js
+++ src/main/resources/static/userAccount.js
@@ -14,51 +14,47 @@ function populateAccounts(accounts) {
   for (account of accounts) {
     let bryDiv = document.createElement("div");
 
-      bryDiv.innerHTML = `
-          <a href="userTransactions.html?accountId=${account.id}" class="text-decoration-none">
-            <div class="card">
-
     bryDiv.innerHTML = `
-            <form>
-
-      bryDiv.innerHTML = `
-            <div>
-
-            <div class="mb-3">
-            <h2 for="savingsAccount" class="form-label">${account.accountType}</h2>
-              <label for="savingsAccount" class="form-label">Account Balance:</label>
-              <label for="savingsAccount" class="form-label">${account.currentBalance}</label>
-            </div>
-            <div class="mb-3">
-              <input type="number" class="form-control" id="account" placeholder="please input amount in $">
-            </div>
-            <button type="submit" class="btn btn-primary" id="withdraw">Withdraw</button>
-            <button type="submit" class="btn btn-primary" id="deposit">Deposit</button>
-            <br>
-            <br>
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Transfer To
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
-            <br>
-            <br>
-            <br>
+    <form>
+    <div class="card p-5 mb-5">
+        <h2>${account.accountType}</h2>
+        <div>
+            <label class="form-label">Account Balance:</label>
+            <span class="form-label">${account.currentBalance}</span>
         </div>
-            
+        <div class="mb-3">
+            <input type="number" class="form-control" id="amountInput" placeholder="Please input amount in $">
+        </div>
+        <div class = "w-50">
+        <button type="submit" class="btn btn-primary mb-2" id="withdraw">Withdraw</button>
+        <button type="submit" class="btn btn-primary" id="deposit">Deposit</button>
+        <div>
+        <br><br>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Transfer To
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </div>
+        <div class = "mt-5">
+        <a href="userTransactions.html?accountId=${account.id}" class="text-decoration-none">
+            View Transactions
+        </a>
+        </div>
+    </div>
+</form> 
         `;
     accountContainer.append(bryDiv);
   }
 }
 
 // Get to user accounts, need to edit this to take in the user who's
-  // logged in
-if (sessionStorage){
+// logged in
+if (sessionStorage) {
   (async () => {
 
     let data = await fetch(
@@ -70,14 +66,6 @@ if (sessionStorage){
     populateAccounts(accounts);
   })();
 } else {
-
-  let data = await fetch(`http://localhost:8080/users/${sessionStorage.getItem("id")}/accounts`);
-  let res = await data.json(); // Different syntax, same thing as before
-  console.log(res);
-  accounts = res;
-  populateAccounts(accounts);
-})();
-}else{
 
   window.location.href = "login.html";
 }
