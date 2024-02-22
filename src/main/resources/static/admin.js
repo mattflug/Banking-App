f3f5aae81842fdac49fbd95d1f5e2096d574aac6
StephanDrawe <stephan.drawe@hotmail.com>
2024-02-21 22:27:53 -0600

    begin search by tax id functionality

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -6,26 +6,30 @@ const url = "http://localhost:8080";
 //     getAllAccount();
 // })
 
+// getAllAccountButton
 const button = document.getElementById("getAllAccountButton");
 button.addEventListener('click', getAllAccount);
 
+// clearAllAccountButton
 const clearButton = document.getElementById("clearAllAccount");
 clearButton.addEventListener('click', clearAccountList);
 
+// searchByTaxButton
 const searchByTaxButton = document.getElementById("searchByTaxButton");
 searchByTaxButton.addEventListener('click', searchByTaxId);
 
-function clearAccountList() {
+
+// clear account view function
+function clearAccountList () {
     let seeAllAccounts = document.getElementById("seeAllAccount");
     seeAllAccounts.innerHTML = "";
     console.log("function fired")
 }
 
-
+// get all account function
 async function getAllAccount () {
   
     let seeAllAccounts = document.getElementById("seeAllAccount");
-    // let errorMessage = document.getElementById("errorMessage");
 
     try { 
         const res = await fetch(`${url}/users`, {
@@ -39,13 +43,40 @@ async function getAllAccount () {
         let stringAllAccounts = JSON.stringify(allAccounts);
         seeAllAccounts.innerHTML = stringAllAccounts;
 
-        // errorMessage.style.display = "none";
         
 
     } catch (e) {
         console.error("Error: " + e);
         console.error("Error!");
-        // errorMessage.style.display = "block";
+    }
+
+}
+
+// search by Tax ID function
+async function searchByTaxId () {
+  
+    let seeTaxAccounts = document.getElementById("seeTaxAccount");
+    let taxErrorMessage = document.getElementById("taxErrorMessage");
+
+    try { 
+        const res = await fetch(`${url}/users/search`, {
+            method: "Get",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const accounts = await res.json();
+        console.log(accounts);
+        let stringAccounts = JSON.stringify(accounts);
+        seeTaxAccounts.innerHTML = stringAccounts;
+
+        taxErrorMessage.style.display = "none";
+        
+
+    } catch (e) {
+        console.error("Error: " + e);
+        console.error("Error!");
+        errorMessage.style.display = "block";
     }
 
 }
