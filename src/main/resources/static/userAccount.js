0417a136b0667fcbb96eaef46d486470fe104971
Ghost <mbflug@gmail.com>
2024-02-22 19:50:06 -0700

    pushing work

--- src/main/resources/static/userAccount.js
+++ src/main/resources/static/userAccount.js
@@ -8,6 +8,37 @@ console.log(accountContainer);
 const logoutButton = document.getElementById("logout");
 logoutButton.addEventListener("click", logoutFunction);
 
+// transfer function
+async function transfer(fromAccountId, toAccountId) {
+  let amount = document.getElementById("amountInput").value;
+  let amountErrorMessage = document.getElementById("amountInputErrorMessage");
+
+  // Validate amount input
+  if (!amount) {
+    amountErrorMessage.style.display = "block";
+    amountErrorMessage.innerHTML = "Please enter a valid amount";
+    return;
+  }
+
+  // Need to validate ac
+
+  try {
+    const res = await fetch(`${url}/accounts/${fromAccountId}/transfer`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        toAccountId,
+        amount,
+      }),
+    });
+  } catch (e) {
+    console.error("Error: " + e);
+    console.error("Error!");
+  }
+}
+
 function populateAccounts(accounts) {
   accountContainer.innerHTML = "";
 
@@ -15,40 +46,53 @@ function populateAccounts(accounts) {
     let bryDiv = document.createElement("div");
 
     bryDiv.innerHTML = `
-    <div>
-    <div class="card p-5 mb-5">
-        <h2>${account.accountType}</h2>
-        <div>
-            <label class="form-label">Account Balance:</label>
-            <span class="form-label">${account.currentBalance}</span>
+    <div class="container container-fluid">
+      <div class="card px-5 pt-3 pb-5 mb-5">
+        <h5>${account.accountType}: ${account.id}</h5>
+        <div class="row mb-3">
+          <div class="col-md-12">
+            <label class="form-label">Account Balance: ${
+              account.currentBalance
+            }</label>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-md-12">
+            <input type="number" class="form-control form-control-sm" id="amountInput" placeholder="Please input amount in $">
+            <p id="amountInputErrorMessage" style="color: red; display: none;"></p>
+          </div>
+        </div>
+        <div class="row mb-1">
+          <div class="col-md-12">
+            <div class="d-grid gap-2 d-md-block">
+              <button type="submit" class="btn btn-sm btn-primary" id="withdraw">Withdraw</button>
+              <button type="submit" class="btn btn-sm btn-primary" id="deposit">Deposit</button>
+            </div>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-md-12">
+            <div class="dropdown">
+              <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Transfer To
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                ${accounts
+                  .filter((a) => a.id !== account.id)
+                  .map(
+                    (a) =>
+                      `<button class="dropdown-item" type="button" id="${a.id}">${a.accountType} ${a.id}</button>`
+                  )}
+              </div>
+           </div>
+          </div>
         </div>
         <div class="mb-3">
-            <input type="number" class="form-control" id="amountInput" placeholder="Please input amount in $">
-        </div>
-        <div class = "w-50">
-        <button type="submit" class="btn btn-primary mb-2" id="withdraw">Withdraw</button>
-        <button type="submit" class="btn btn-primary" id="deposit">Deposit</button>
-        <div>
-        <br><br>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Transfer To
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-   ${accounts
-     .filter((a) => a.id !== account.id)
-     .map(
-       (a) =>
-         `<button class="dropdown-item" type="button">${a.accountType} ${a.id}</button>`
-     )}
-            </div>
-        </div>
-        <div class = "mt-5">
-        <a href="userTransactions.html?accountId=${
-          account.id
-        }" class="text-decoration-none">
-            View Transactions
-        </a>
+          <a href="userTransactions.html?accountId=${
+            account.id
+          }" class="text-decoration-none">
+              View Transactions
+          </a>
         </div>
     </div>
 </div> 
@@ -57,7 +101,6 @@ function populateAccounts(accounts) {
   }
 }
 
-
 //on load userAccount page for currently logged in user
 // if (sessionStorage){
 // }
@@ -65,7 +108,6 @@ function populateAccounts(accounts) {
 // logged in
 if (sessionStorage) {
   (async () => {
-
     let data = await fetch(
       `http://localhost:8080/users/${sessionStorage.getItem("id")}/accounts`
     );
@@ -75,7 +117,6 @@ if (sessionStorage) {
     populateAccounts(accounts);
   })();
 } else {
-
   window.location.href = "login.html";
 }
 
@@ -84,52 +125,7 @@ function logoutFunction() {
 }
 
 // Withdraw function
-async function withdraw (id) {
-
-    console.log(id);
-  
-    let amount = document.getElementById("input-" + id).value;
-
-    console.log(amount);
-
-    let errorMessage = document.getElementById("ErrorMessage-" + id);
-
-
-    if(!amount){
-        errorMessage.style.display = "block";
-        errorMessage.innerHTML = "Please enter an amount.";
-        return;
-    }
-
-    try { 
-        const res = await fetch(`${url}/accounts/${id}/withdraw`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            amount: amount
-          }),
-        });
-        const response = await res.json();
-        console.log(response);
-        let jsonResponse = JSON.stringify(response);
-        console.log(jsonResponse);
-        errorMessage.style.display = "none";
-        window.location.href = "userAccount.html";
-        
-    } catch (e) {
-        console.error("Error: " + e);
-        console.error("Error!");
-        errorMessage.style.display = "block";
-        errorMessage.innerHTML = "Insufficient funds";
-    }
-
-}
-
-// Deposit function
-async function deposit (id) {
-
+async function withdraw(id) {
   console.log(id);
 
   let amount = document.getElementById("input-" + id).value;
@@ -138,34 +134,72 @@ async function deposit (id) {
 
   let errorMessage = document.getElementById("ErrorMessage-" + id);
 
-
-  if(!amount){
-      errorMessage.style.display = "block";
-      errorMessage.innerHTML = "Please enter an amount.";
-      return;
+  if (!amount) {
+    errorMessage.style.display = "block";
+    errorMessage.innerHTML = "Please enter an amount.";
+    return;
   }
 
-  try { 
-      const res = await fetch(`${url}/accounts/${id}/deposit`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          amount: amount
-        }),
-      });
-      const response = await res.json();
-      console.log(response);
-      let jsonResponse = JSON.stringify(response);
-      console.log(jsonResponse);
-      errorMessage.style.display = "none";
-      window.location.href = "userAccount.html";
-      
+  try {
+    const res = await fetch(`${url}/accounts/${id}/withdraw`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        amount: amount,
+      }),
+    });
+    const response = await res.json();
+    console.log(response);
+    let jsonResponse = JSON.stringify(response);
+    console.log(jsonResponse);
+    errorMessage.style.display = "none";
+    window.location.href = "userAccount.html";
   } catch (e) {
-      console.error("Error: " + e);
-      console.error("Error!");
-      errorMessage.style.display = "block";
-      errorMessage.innerHTML = "Insufficient funds";
+    console.error("Error: " + e);
+    console.error("Error!");
+    errorMessage.style.display = "block";
+    errorMessage.innerHTML = "Insufficient funds";
+  }
+}
+
+// Deposit function
+async function deposit(id) {
+  console.log(id);
+
+  let amount = document.getElementById("input-" + id).value;
+
+  console.log(amount);
+
+  let errorMessage = document.getElementById("ErrorMessage-" + id);
+
+  if (!amount) {
+    errorMessage.style.display = "block";
+    errorMessage.innerHTML = "Please enter an amount.";
+    return;
+  }
+
+  try {
+    const res = await fetch(`${url}/accounts/${id}/deposit`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        amount: amount,
+      }),
+    });
+    const response = await res.json();
+    console.log(response);
+    let jsonResponse = JSON.stringify(response);
+    console.log(jsonResponse);
+    errorMessage.style.display = "none";
+    window.location.href = "userAccount.html";
+  } catch (e) {
+    console.error("Error: " + e);
+    console.error("Error!");
+    errorMessage.style.display = "block";
+    errorMessage.innerHTML = "Insufficient funds";
   }
 }
